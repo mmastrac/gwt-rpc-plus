90eb5ba214035fb707a9995b99b4f9653360b58f
mmastrac <mmastrac@1d15933a-82c5-11de-aa24-d199ae5efac3>
2009-08-10 23:17:16 +0000

    - Removing CallResponseProcessor  - Adding CallInterceptor to allow code to run before/after requests  - Fix FlexibleRemoteServiceServlet for compilation against GWT 1.7

--- gwt-rpc-plus/example/gen-java-gwt/com/dotspots/rpcplus/example/torturetest/client/TortureTestApi.java
+++ gwt-rpc-plus/example/gen-java-gwt/com/dotspots/rpcplus/example/torturetest/client/TortureTestApi.java
@@ -7,52 +7,34 @@ import com.dotspots.rpcplus.client.jsonrpc.*;
 import com.dotspots.rpcplus.client.jscollections.*;
 
 @SuppressWarnings("unused")
-public final class TortureTestApi extends com.dotspots.rpcplus.client.jsonrpc.thrift.ThriftClientStub implements CallResponseProcessor {
-    private ContextIn requestContext;
-    private ContextOut responseContext;
-
-    private ContextIn popRequestContext() {
-        ContextIn requestContext = this.requestContext;
-        this.requestContext = null;
-        return requestContext;
-    }
-
+public final class TortureTestApi extends com.dotspots.rpcplus.client.jsonrpc.thrift.ThriftClientStub<TortureTestApi> {
     public void setRequestContext(ContextIn requestContext) {
         this.requestContext = requestContext;
     }
 
     public ContextOut popResponseContext() {
-        ContextOut responseContext = this.responseContext;
+        ContextOut responseContext = this.responseContext.cast();
         this.responseContext = null;
         return responseContext;
     }
 
-    @SuppressWarnings("unchecked")
-    public void onResponse(int methodId, AsyncCallback<?> asyncCallback, JavaScriptObject rawResponse) {
-        CallResponse<?> response = callDecoder.decodeCall(rawResponse);
-        this.responseContext = response.getResponseContext().cast();
-        int responseCode = response.getResponseCode();
-        if (responseCode == 0) {
-            ((AsyncCallback<JavaScriptObject>)asyncCallback).onSuccess(response.getResponseObject().getFieldValue(0));
-            return;
-        }
-
+    public void onException(int methodId, AsyncCallback<?> asyncCallback, int responseCode, BaseJsRpcObject response) {
         // Process exceptions per method
         switch (responseCode * 14 + methodId) {
         case 14:
-            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.SimpleException(response.getResponseObject().getFieldValue(1)));
+            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.SimpleException(response.getFieldValue(1)));
             break;
         case 15:
-            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.SimpleException(response.getResponseObject().getFieldValue(1)));
+            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.SimpleException(response.getFieldValue(1)));
             break;
         case 16:
-            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.SimpleException(response.getResponseObject().getFieldValue(1)));
+            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.SimpleException(response.getFieldValue(1)));
             break;
         case 17:
-            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.SimpleException(response.getResponseObject().getFieldValue(1)));
+            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.SimpleException(response.getFieldValue(1)));
             break;
         case 31:
-            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.MoreComplexException(response.getResponseObject().getFieldValue(2)));
+            asyncCallback.onFailure(new com.dotspots.rpcplus.example.torturetest.client.MoreComplexException(response.getFieldValue(2)));
             break;
         default:
             asyncCallback.onFailure(new com.dotspots.rpcplus.client.jsonrpc.RpcException("Unknown exception"));
@@ -61,59 +43,59 @@ public final class TortureTestApi extends com.dotspots.rpcplus.client.jsonrpc.th
     }
 
     public void testThrowsAnException(AsyncCallback<String> callback) {
-        call(0, "testThrowsAnException", TortureTestApi_testThrowsAnException_args.create(), popRequestContext(), callback);
+        call(0, "testThrowsAnException", TortureTestApi_testThrowsAnException_args.create(), callback);
     };
 
     public void testThrowsAnUnpositionedException(AsyncCallback<String> callback) {
-        call(1, "testThrowsAnUnpositionedException", TortureTestApi_testThrowsAnUnpositionedException_args.create(), popRequestContext(), callback);
+        call(1, "testThrowsAnUnpositionedException", TortureTestApi_testThrowsAnUnpositionedException_args.create(), callback);
     };
 
     public void testDeclaresAnException(AsyncCallback<String> callback) {
-        call(2, "testDeclaresAnException", TortureTestApi_testDeclaresAnException_args.create(), popRequestContext(), callback);
+        call(2, "testDeclaresAnException", TortureTestApi_testDeclaresAnException_args.create(), callback);
     };
 
     public void testThrowsTwoExceptions(AsyncCallback<String> callback) {
-        call(3, "testThrowsTwoExceptions", TortureTestApi_testThrowsTwoExceptions_args.create(), popRequestContext(), callback);
+        call(3, "testThrowsTwoExceptions", TortureTestApi_testThrowsTwoExceptions_args.create(), callback);
     };
 
     public void testExceptionPassthru(com.dotspots.rpcplus.example.torturetest.client.SimpleException ex, AsyncCallback<com.dotspots.rpcplus.example.torturetest.client.SimpleException> callback) {
-        call(4, "testExceptionPassthru", TortureTestApi_testExceptionPassthru_args.create(ex), popRequestContext(), callback);
+        call(4, "testExceptionPassthru", TortureTestApi_testExceptionPassthru_args.create(ex), callback);
     };
 
     public void testPositionalArguments(int int32, String str, AsyncCallback<String> callback) {
-        call(5, "testPositionalArguments", TortureTestApi_testPositionalArguments_args.create(int32, str), popRequestContext(), callback);
+        call(5, "testPositionalArguments", TortureTestApi_testPositionalArguments_args.create(int32, str), callback);
     };
 
     public void testSetString(AsyncCallback<JsRpcSetString> callback) {
-        call(6, "testSetString", TortureTestApi_testSetString_args.create(), popRequestContext(), callback);
+        call(6, "testSetString", TortureTestApi_testSetString_args.create(), callback);
     };
 
     public void testSetInt(AsyncCallback<JsRpcSetInt> callback) {
-        call(7, "testSetInt", TortureTestApi_testSetInt_args.create(), popRequestContext(), callback);
+        call(7, "testSetInt", TortureTestApi_testSetInt_args.create(), callback);
     };
 
     public void testMapStringString(AsyncCallback<JsRpcMapString<String>> callback) {
-        call(8, "testMapStringString", TortureTestApi_testMapStringString_args.create(), popRequestContext(), callback);
+        call(8, "testMapStringString", TortureTestApi_testMapStringString_args.create(), callback);
     };
 
     public void methodReturningAnObject(AsyncCallback<com.dotspots.rpcplus.example.torturetest.client.ObjectThatReferencesAnother> callback) {
-        call(9, "methodReturningAnObject", TortureTestApi_methodReturningAnObject_args.create(), popRequestContext(), callback);
+        call(9, "methodReturningAnObject", TortureTestApi_methodReturningAnObject_args.create(), callback);
     };
 
     public void methodReturningAnObject2(AsyncCallback<com.dotspots.rpcplus.example.torturetest.client.SimpleObjectWithFieldIds> callback) {
-        call(10, "methodReturningAnObject2", TortureTestApi_methodReturningAnObject2_args.create(), popRequestContext(), callback);
+        call(10, "methodReturningAnObject2", TortureTestApi_methodReturningAnObject2_args.create(), callback);
     };
 
     public void methodReturningAnObject3(AsyncCallback<com.dotspots.rpcplus.example.torturetest.client.SimpleObjectWithNoFieldIds> callback) {
-        call(11, "methodReturningAnObject3", TortureTestApi_methodReturningAnObject3_args.create(), popRequestContext(), callback);
+        call(11, "methodReturningAnObject3", TortureTestApi_methodReturningAnObject3_args.create(), callback);
     };
 
     public void methodReturningAnObject4(AsyncCallback<com.dotspots.rpcplus.example.torturetest.client.ObjectWithComplexTypes> callback) {
-        call(12, "methodReturningAnObject4", TortureTestApi_methodReturningAnObject4_args.create(), popRequestContext(), callback);
+        call(12, "methodReturningAnObject4", TortureTestApi_methodReturningAnObject4_args.create(), callback);
     };
 
     public void testBinary(String binaryValue, AsyncCallback<String> callback) {
-        call(13, "testBinary", TortureTestApi_testBinary_args.create(binaryValue), popRequestContext(), callback);
+        call(13, "testBinary", TortureTestApi_testBinary_args.create(binaryValue), callback);
     };
 
 }
