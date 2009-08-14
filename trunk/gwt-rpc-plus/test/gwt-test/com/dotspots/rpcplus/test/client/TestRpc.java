fcf9f7df7383a2dbc415561bca849eb79ad21324
mmastrac <mmastrac@1d15933a-82c5-11de-aa24-d199ae5efac3>
2009-08-14 01:43:43 +0000

    Now that FF1.5 is out of the mix, test CLEAR_CACHE as the default again

--- trunk/gwt-rpc-plus/test/gwt-test/com/dotspots/rpcplus/test/client/TestRpc.java
+++ trunk/gwt-rpc-plus/test/gwt-test/com/dotspots/rpcplus/test/client/TestRpc.java
@@ -110,8 +110,7 @@ public class TestRpc extends GWTTestCase {
 		WindowNameTransport transport = new WindowNameTransport();
 		transport.setUrl(getCrossSiteModuleBaseUrl() + "/api");
 		transport.setDocument(Document.get());
-		// Removing this to test
-		// transport.setRedirectFavicon();
+		transport.setRedirectFavicon();
 
 		api.setTransport(new JsonOverTextTransport(transport, new EvalJsonDecoder(getWindow()), new JSONObjectJsonEncoder()));
 		api.setRequestContext(ContextIn.create("token", "\"'*(,.&^<>!@#5$%()^\\\n"));
