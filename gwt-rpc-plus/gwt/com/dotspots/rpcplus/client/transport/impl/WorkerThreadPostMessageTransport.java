8be4ea7297f6b2035a90a10a8abb54713926af8c
mmastrac <mmastrac@1d15933a-82c5-11de-aa24-d199ae5efac3>
2009-12-01 22:48:39 +0000

    Adding work-in-progress web worker-thread transport.

---
+++ gwt-rpc-plus/gwt/com/dotspots/rpcplus/client/transport/impl/WorkerThreadPostMessageTransport.java
@@ -0,0 +1,43 @@
+package com.dotspots.rpcplus.client.transport.impl;
+
+import com.dotspots.rpcplus.client.jscollections.JsRpcMapString;
+import com.dotspots.rpcplus.client.transport.TextTransport;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * Creates a transport that sends message from a host page to a web worker, or from a web worker to a host page
+ * (http://www.whatwg.org/specs/web-workers/current-work/).
+ */
+public class WorkerThreadPostMessageTransport implements TextTransport {
+	protected JavaScriptObject worker;
+	protected JsRpcMapString<WorkerThreadPostMessageTransportRequest> inflightRequests = JsRpcMapString.create();
+
+	/**
+	 * Creates a transport against 'self' (from Worker to host)
+	 */
+	public WorkerThreadPostMessageTransport() {
+		this(getSelf0());
+	}
+
+	/**
+	 * Creates a transport against a given worker.
+	 */
+	public WorkerThreadPostMessageTransport(JavaScriptObject worker) {
+	}
+
+	public void call(String arguments, AsyncCallback<String> callback) {
+		WorkerThreadPostMessageTransportRequest transportRequest = new WorkerThreadPostMessageTransportRequest(this, arguments, callback);
+		transportRequest.start();
+	}
+
+	/**
+	 * Self is the global object in a worker context.
+	 * 
+	 * @return
+	 */
+	private static native JavaScriptObject getSelf0() /*-{
+		return self;
+	}-*/;
+
+}
