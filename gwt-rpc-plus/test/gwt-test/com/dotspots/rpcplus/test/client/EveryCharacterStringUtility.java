59a7e365f9120dde28393a909dcaeef764337ffe
mmastrac <mmastrac@1d15933a-82c5-11de-aa24-d199ae5efac3>
2009-12-12 04:17:26 +0000

    Don't test the invalid unicode characters - they won't work on every browser.

--- gwt-rpc-plus/test/gwt-test/com/dotspots/rpcplus/test/client/EveryCharacterStringUtility.java
+++ gwt-rpc-plus/test/gwt-test/com/dotspots/rpcplus/test/client/EveryCharacterStringUtility.java
@@ -13,23 +13,28 @@ public class EveryCharacterStringUtility {
 	public static String getAllCharacterString() {
 		final StringBuilder stringBuilder = new StringBuilder();
 		for (int i = START_CHAR; i <= END_CHAR; i++) {
-			stringBuilder.append((char) i);
+			if (i < Character.MIN_SURROGATE || i > Character.MAX_SURROGATE) {
+				stringBuilder.append((char) i);
+			}
 		}
 
 		return stringBuilder.toString();
 	}
 
 	public static void checkString(String comparison) {
+		int index = 0;
 		for (int i = 0; i < comparison.length(); i++) {
 			final int expected = i + START_CHAR;
-			final int actual = comparison.charAt(i);
-			if (actual != expected) {
-				Assert.fail("Comparison failed at index " + i + ": character " + charToHex(expected) + " was " + charToHex(actual)
-						+ " instead.  Length of comparsion was " + comparison.length());
+			if (i < Character.MIN_SURROGATE || i > Character.MAX_SURROGATE) {
+				final int actual = comparison.charAt(index++);
+				if (actual != expected) {
+					Assert.fail("Comparison failed at index " + i + ": character " + charToHex(expected) + " was " + charToHex(actual)
+							+ " instead.  Length of comparsion was " + comparison.length());
+				}
 			}
 		}
 
-		if (comparison.length() != END_CHAR - START_CHAR + 1) {
+		if (comparison.length() != END_CHAR - START_CHAR + 1 - (Character.MAX_SURROGATE - Character.MIN_SURROGATE + 1)) {
 			Assert.fail("Comparison was truncated at index " + comparison.length());
 		}
 	}
