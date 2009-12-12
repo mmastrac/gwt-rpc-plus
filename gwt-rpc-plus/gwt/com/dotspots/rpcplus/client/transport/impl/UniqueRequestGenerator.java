cde145b9c158775e9b7bfb8b31adf78f36b6257c
mmastrac <mmastrac@1d15933a-82c5-11de-aa24-d199ae5efac3>
2009-12-12 05:22:43 +0000

    Make the request ID more readable.

--- gwt-rpc-plus/gwt/com/dotspots/rpcplus/client/transport/impl/UniqueRequestGenerator.java
+++ gwt-rpc-plus/gwt/com/dotspots/rpcplus/client/transport/impl/UniqueRequestGenerator.java
@@ -2,12 +2,14 @@ package com.dotspots.rpcplus.client.transport.impl;
 
 import com.google.gwt.core.client.Duration;
 
+/**
+ * Globally-unique request ID generator.
+ */
 public class UniqueRequestGenerator {
 	// Globally unique ID
 	private static int nameSerial;
 
 	public static String createUniqueId() {
-		return nameSerial++ + "-" + Duration.currentTimeMillis();
+		return nameSerial++ + "-" + (Duration.currentTimeMillis() % 0xffffff);
 	}
-
 }
