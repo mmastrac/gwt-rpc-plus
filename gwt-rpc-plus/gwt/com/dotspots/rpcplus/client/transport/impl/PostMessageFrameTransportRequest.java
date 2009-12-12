4ab346684f93e0ea78a4c1f516e88312b83fbb98
mmastrac <mmastrac@1d15933a-82c5-11de-aa24-d199ae5efac3>
2009-12-12 03:55:52 +0000

    Checked in the wrong postmessage transport. Also missed a change in the cross-domain transport factory.

---
+++ gwt-rpc-plus/gwt/com/dotspots/rpcplus/client/transport/impl/PostMessageFrameTransportRequest.java
@@ -0,0 +1,66 @@
+package com.dotspots.rpcplus.client.transport.impl;
+
+import com.dotspots.rpcplus.client.dom.PostMessageEvent;
+import com.dotspots.rpcplus.client.dom.RpcWindow;
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * Provides the postMessage part of cross-domain frame requests.
+ */
+public class PostMessageFrameTransportRequest extends CrossDomainFrameTransportRequest {
+	private JavaScriptObject handler;
+	private RpcWindow receiveWindow;
+	private RpcWindow sendWindow;
+
+	public PostMessageFrameTransportRequest(String arguments, AsyncCallback<String> callback, Document document, String url, int timeout) {
+		super(arguments, callback, document, url, timeout);
+		this.receiveWindow = RpcWindow.fromDocument(document);
+	}
+
+	@Override
+	protected String getRequestType() {
+		return "postmessage";
+	}
+
+	@Override
+	protected void attachIFrameListeners() {
+		handler = addReceivePostMessageHandler(receiveWindow);
+	}
+
+	@Override
+	protected void detachIFrameListeners() {
+		removeReceivePostMessageHandler(receiveWindow, handler);
+	}
+
+	protected void onMessage(PostMessageEvent e) {
+		this.sendWindow = RpcWindow.fromIFrame(iframe);
+		if (equals0(e.getSource(), sendWindow)) {
+			callback.onSuccess(e.getData());
+		}
+	}
+
+	private native boolean equals0(RpcWindow w1, RpcWindow w2) /*-{
+		return w1 == w2;
+	}-*/;
+
+	protected native JavaScriptObject addReceivePostMessageHandler(RpcWindow receiveWindow) /*-{
+		var self = this;
+		var handler = function(e) { self.@com.dotspots.rpcplus.client.transport.impl.PostMessageFrameTransportRequest::onMessage(Lcom/dotspots/rpcplus/client/dom/PostMessageEvent;)(e); };
+
+		if (receiveWindow.addEventListener)
+		receiveWindow.addEventListener('message', handler, true);
+		else
+		receiveWindow.attachEvent('onmessage', handler);
+
+		return handler;
+	}-*/;
+
+	protected native void removeReceivePostMessageHandler(RpcWindow receiveWindow, JavaScriptObject handler) /*-{
+		if (receiveWindow.removeEventListener)
+		receiveWindow.removeEventListener('message', handler, true);
+		else
+		receiveWindow.detachEvent('onmessage', handler);
+	}-*/;
+}
